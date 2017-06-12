6ec41a29f0975f87851d0716355024aeae961cca
sdanko <danko.simunovic@edu.fit.ba>
2017-06-12 15:18:31 +0200

    Izmjene

--- src/main/java/com/urlshortener/user/UserController.java
+++ src/main/java/com/urlshortener/user/UserController.java
@@ -1,8 +1,8 @@
 package com.urlshortener.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
@@ -13,4 +13,10 @@ public class UserController {
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+//    @RequestMapping(method = RequestMethod.POST)
+//    ResponseEntity<?> add(@RequestBody String accountId) {
+//
+//
+//    }
 }
