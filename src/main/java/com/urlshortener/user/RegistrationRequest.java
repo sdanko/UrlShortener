07669e9c9459e133d68cebfcced019d406e7a7d0
sdanko <danko.simunovic@edu.fit.ba>
2017-06-15 15:43:12 +0200

    Dorada modela za kreiranje korisnika

--- src/main/java/com/urlshortener/user/RegistrationRequest.java
+++ src/main/java/com/urlshortener/user/RegistrationRequest.java
@@ -1,10 +1,12 @@
 package com.urlshortener.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 //Model za objekat koji se salje pri registraciji naloga
 public class RegistrationRequest {
     private final String AccountId;
 
-    public RegistrationRequest(String accountId) {
+    public RegistrationRequest(@JsonProperty("AccountId")String accountId) {
         AccountId = accountId;
     }
 
